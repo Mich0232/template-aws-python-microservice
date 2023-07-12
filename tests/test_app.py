466d1dba0ab8d8da8f3b40f2ecb002ebb17f6e4d
michaldrozdz <mich0232@gmail.com>
2023-07-12 10:34:47 +0200

    Pytest setup

---
+++ tests/test_app.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+import pytest
+
+from src import app
+
+
+@pytest.fixture
+def lambda_context():
+    @dataclass
+    class LambdaContext:
+        function_name: str = "test"
+        memory_limit_in_mb: int = 128
+        invoked_function_arn: str = (
+            "arn:aws:lambda:eu-west-1:123456789012:function:test"
+        )
+        aws_request_id: str = "da658bd3-2d6f-4e7b-8ec2-937234644fdc"
+
+    return LambdaContext()
+
+
+def test_lambda_handler(lambda_context):
+    minimal_event = {
+        "rawPath": "/hello",
+        "requestContext": {
+            "requestContext": {
+                "requestId": "227b78aa-779d-47d4-a48e-ce62120393b8"
+            },  # correlation ID
+            "http": {
+                "method": "GET",
+            },
+            "stage": "$default",
+        },
+    }
+    # Example of API Gateway HTTP API request event:
+    # https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-develop-integrations-lambda.html
+
+    ret = app.lambda_handler(minimal_event, lambda_context)
+    assert ret["statusCode"] == 200
+    assert ret["body"] == '{"message":"hello unknown!"}'
